ea49763bca668f78a6932d687b1b583cc376011d
Daniel Selig <daniel.selig@swimlane.com>
2022-01-27 07:44:48 -0600

    Make the algorithm a little smarter when there are only a few valid guesses left

--- loop_all.py
+++ loop_all.py
@@ -13,13 +13,25 @@ for w in valid_words:
     attempts = []
     while True:
         with GuessGenerator(*attempts) as gg:
-            if len(gg.wordle.valid) == 1:
-                assert(o.guess(str(gg.wordle.valid[0])) == (2, 2, 2, 2, 2))
-                guesses[w].append(str(gg.wordle.valid[0]))
+            best = None
+            for g in gg:
+                if g.entropy > 0:
+                    if lastg is None:
+                        best = g
+                    break
+                elif str(g.word) in valid_words:
+                    best = g
+                    break
+                else:
+                    best = g
+            response = o.guess(str(best.word))
+            if response == (2, 2, 2, 2, 2):
+                guesses[w].append(str(best.word))
                 print(f'{w} guessed in {len(guesses[w])} attempts')
                 break
-            g = next(gg)
-            attempts.append(f'{g.word}:{"".join([str(x) for x in o.guess(str(g.word))])}')
-            guesses[w].append(str(g.word))
+            else:
+                attempts.append(f'{g.word}:{"".join([str(x) for x in o.guess(str(g.word))])}')
+                guesses[w].append(str(g.word))
+                    
 with open('all_guesses.json', 'w') as f:
     json.dump(guesses, f, indent=True)
